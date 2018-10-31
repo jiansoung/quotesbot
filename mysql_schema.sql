3296a2e781f173f515031232bfd5382a0ad53b79
jiansoung <Jian.Soung@outlook.com>
2018-11-01 00:54:10 +0800

    Create MySQL Schema File `mysql_schema.sql` for spider `quotesbot`

---
+++ mysql_schema.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE IF NOT EXISTS quotesbot CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+USE quotesbot;
+
+CREATE TABLE IF NOT EXISTS authors (
+    id          INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name        VARCHAR(255) NOT NULL UNIQUE,
+    image_path  VARCHAR(255) DEFAULT '',
+    created_at  DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at  DATETIME ON UPDATE CURRENT_TIMESTAMP
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS tags (
+    id          INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name         VARCHAR(255) NOT NULL UNIQUE,
+    created_at  DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at  DATETIME ON UPDATE CURRENT_TIMESTAMP
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS quotes (
+    id          INTEGER PRIMARY KEY AUTO_INCREMENT,
+    text        VARCHAR(512) NOT NULL,
+    author_id   INTEGER NOT NULL,
+    created_at  DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at  DATETIME ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (author_id) REFERENCES authors(id),
+    UNIQUE (author_id, text)
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS quote_tag_assoc (
+    id          INTEGER PRIMARY KEY AUTO_INCREMENT,
+    quote_id    INTEGER NOT NULL,
+    tag_id      INTEGER NOT NULL,
+    created_at  DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at  DATETIME ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (quote_id) REFERENCES quotes(id),
+    FOREIGN KEY (tag_id) REFERENCES tags(id),
+    UNIQUE (quote_id, tag_id)
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
